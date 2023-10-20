d2362e47a72c7b9d9b0939f5619eec342a19f5f1
chimekinglsey <onlinekingsley@gmail.com>
2023-10-20 15:20:49 +0100

    simple pagination index calculator

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,8 +8,8 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple:
     """Returns a tuples matching the range of indexes """
     if page <= 0:
-        new_page = page - 1
-        return(new_page, page_size * (new_page + 1))
+        new_page = 0
+        return(new_page, page_size)
     else:
         new_page = page - 1
         return(new_page * page_size, page_size * page)
