56711e0c770e2673e48fdc22d1e36eadd652b8af
chimekinglsey <onlinekingsley@gmail.com>
2023-10-21 11:25:30 +0100

    A function that paginates a csv dataset, returns paginated data or [] if empty

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -42,19 +42,22 @@ class Server:
         file = self.dataset()
         assert page > 0 and type(page) == int
         assert type(page_size) == int
+        # assert page_size > 0
 
         result = index_range(page, page_size)
         start_index = result[0]
         stop_index = result[1]
 
+        count, return_list = 0, []
         if start_index <= file_size and stop_index <= file_size:
-            count, return_list = 0, []
             while count < stop_index:
                 if start_index < file_size:
                     return_list.append(file[start_index])
                     start_index += 1
+
                 count += 1
-            return return_list
+        return return_list
+ 
 #         pass
 # data = Server()
 # print(data.get_page(6, 4))
