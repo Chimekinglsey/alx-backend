3c54d55bac12525575bfd3823d29c1cface6cfeb
chimekinglsey <onlinekingsley@gmail.com>
2023-10-24 23:52:00 +0100

    Last-In-First-Out cache implementation

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+1. FIFO Caching
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching as Base
+
+
+class LIFOCache(Base):
+    """A caching system that removes items from cache Last-In-First-Out """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """sets new key and item to cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                discard_key = self.cache_data.popitem(last=True)
+                print(f"DISCARD: {discard_key[0]}")
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ retrieves a key if present in cache """
+        if key is None or not self.cache_data.get(key):
+            return None
+        return self.cache_data[key]
