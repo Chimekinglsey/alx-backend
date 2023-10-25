cf7951d833ced3d6ddfbfc998e98e21c5a907c4b
chimekinglsey <onlinekingsley@gmail.com>
2023-10-25 23:14:36 +0100

    lfu caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -16,35 +16,29 @@ class LFUCache(BaseCaching):
         self.keys_usage = {}
         self.usage_keys = {}
         self.counter = 0
+        self.min_usage = 1  # Initialize min_usage
 
     def put(self, key, item):
         """ Add an item to the cache
         """
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                keys_to_discard = []
-                min_usage = min(self.keys_usage.values())
-                for k, v in self.keys_usage.items():
-                    if v == min_usage:
-                        keys_to_discard.append(k)
-                if len(keys_to_discard) > 1:
-                    lru_key = min(self.usage_keys[min_usage], key=lambda x:
-                                  self.usage_keys[min_usage][x])
-                    keys_to_discard.remove(lru_key)
-                for k in keys_to_discard:
-                    del self.cache_data[k]
-                    del self.keys_usage[k]
-                    del self.usage_keys[min_usage][k]
-                    print(f"DISCARD: {k}")
+                while self.min_usage not in self.usage_keys or\
+                        not self.usage_keys[self.min_usage]:
+                    self.min_usage += 1
+
+                min_usage_keys = self.usage_keys[self.min_usage]
+                lru_key = min(min_usage_keys, key=lambda k: min_usage_keys[k])
+                print(f"DISCARD: {lru_key}")
+                del self.cache_data[lru_key]
+                del self.keys_usage[lru_key]
+                del self.usage_keys[self.min_usage][lru_key]
+
             self.cache_data[key] = item
-            if key in self.keys_usage:
-                self.keys_usage[key] += 1
-            else:
-                self.keys_usage[key] = 1
-            if 1 not in self.usage_keys:
-                self.usage_keys[1] = {}
-            self.usage_keys[1][key] = self.counter
+            self.keys_usage[key] = 1
+            self.usage_keys.setdefault(1, {})[key] = self.counter
             self.counter += 1
+            self.min_usage = 1  # Reset min_usage after adding a new item
 
     def get(self, key):
         """ Get an item from the cache
@@ -52,10 +46,11 @@ class LFUCache(BaseCaching):
         if key is not None and key in self.cache_data:
             self.keys_usage[key] += 1
             usage = self.keys_usage[key]
-            del self.usage_keys[usage - 1][key]
-            if usage not in self.usage_keys:
-                self.usage_keys[usage] = {}
-            self.usage_keys[usage][key] = self.counter
+            old_usage = usage - 1
+            del self.usage_keys[old_usage][key]
+            if old_usage not in self.usage_keys:
+                self.min_usage = old_usage + 1
+            self.usage_keys.setdefault(usage, {})[key] = self.counter
             self.counter += 1
             return self.cache_data[key]
         return None
