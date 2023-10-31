6186148b3d681d5f43bb1ddd9ce01b9671c777c5
chimekinglsey <onlinekingsley@gmail.com>
2023-10-31 20:15:21 +0100

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """
- 0. Basic Flask app 
+ 0. Basic Flask app
 """
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     """Home Page"""
