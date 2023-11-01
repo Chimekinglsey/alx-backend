3fc6237a68613b5a743bf753b01e5ba4e30ba7ad
chimekinglsey <onlinekingsley@gmail.com>
2023-11-01 02:25:36 +0100

    determine locale from url args

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -23,6 +23,9 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale() -> str:
     """Determine the best match from supported languages"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
